831007495473496af693109b0baf30cb8bf40631
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2024-11-18 15:31:13 +0000

    fix: Remove tegra builds; no longer supported by NDK.
    References:
    
    - https://outsystemsrd.atlassian.net/browse/RMET-3602
    - https://outsystemsrd.atlassian.net/wiki/spaces/RDME/pages/4426137659/iOS+18+Android+15+Assessment

--- card.io/src/main/jni/Android.mk
+++ card.io/src/main/jni/Android.mk
@@ -51,36 +51,6 @@ endif
 
 endif
 
-# build tegra compatible lib
-# (no neon, limit to 16 VFP registers)
-
-ifeq (1,1)
-
-include $(CLEAR_VARS)
-ifneq (,$(filter $(TARGET_ARCH_ABI),armeabi-v7a x86 arm64-v8a x86_64))
-
-LOCAL_MODULE := cardioRecognizer_tegra2
-LOCAL_LDLIBS := -llog -L$(SYSROOT)/usr/lib -lz -ljnigraphics
-LOCAL_SHARED_LIBRARIES := cpufeatures opencv_imgproc opencv_core 
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(LOCAL_DMZ_DIR) $(LOCAL_PATH)/$(LOCAL_DMZ_DIR)/cv
-LOCAL_SRC_FILES := $(LOCAL_DMZ_DIR)/dmz_all.cpp nativeRecognizer.cpp
-
-ifeq ($(TARGET_ARCH_ABI), x86) #we're generating an empty libcardioRecognizer_tegra2.so for x86 devices, so the list of .so files is the same for armeabi-v7a and x86 folders. This is to avoid any fallback to arm versions.
-LOCAL_C_INCLUDES :=
-LOCAL_SRC_FILES :=
-endif
-
-# Note: setting -mfloat-abi=hard will generate libs that cannot be linked with built in Android ones. So don't.
-LOCAL_CPPFLAGS := -DANDROID_HAS_NEON=0 -mfpu=vfpv3-d16
-LOCAL_ARM_NEON := false
-
-include $(BUILD_SHARED_LIBRARY)
-
-endif
-
-endif
-
 # --- libcardioDecider.so ------------------------------------------------------------
 
 ifneq (,$(filter $(TARGET_ARCH_ABI), armeabi armeabi-v7a mips x86 arm64-v8a x86_64))
